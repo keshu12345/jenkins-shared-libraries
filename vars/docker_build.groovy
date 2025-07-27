67d692cd0d1f727ef73e22f913d7e8fbe25dc669
Keshav Rajput <57296870+keshu12345@users.noreply.github.com>
2025-07-27 14:40:59 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,13 +1,5 @@
-def call(String credId, String imageName){
-  withCredentials([usernamePassword(
-                    credentialsId:"${credId}",
-                    passwordVariable: "dockerHubPass",
-                    usernameVariable: "dockerHubUser"
-                )]){
-                
-                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                sh "docker image tag ${imageName} ${env.dockerHubUser}/${imageName}"
-                sh "docker push ${env.dockerHubUser}/${imageName}:latest"
-            
-                }  
+
+def call(String projectName, String imageTag , String dockerHubUser){
+   sh "dokcer build -t "${dockerHubUser}/${projectName}:${inageTag}"
+ 
 }
